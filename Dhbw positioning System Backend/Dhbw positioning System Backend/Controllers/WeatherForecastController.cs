83415ecfa9226206f57e411bc0528c95cfe143e0
Leon Neumann <lneumann@allegion.com>
2022-10-27 11:10:19 +0200

    Added SQLiteDB

--- Dhbw positioning System Backend/Dhbw positioning System Backend/Controllers/WeatherForecastController.cs
+++ Dhbw positioning System Backend/Dhbw positioning System Backend/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace Dhbw_positioning_System_Backend.Controllers
 {
@@ -26,6 +27,15 @@ namespace Dhbw_positioning_System_Backend.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            //SQLiteConnection m_dbConnection;
+            //m_dbConnection =
+            //    new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            //m_dbConnection.Open();
+            //string sql = "insert into highscores (name, score) values ('Me', 3000)";
+            //SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            //command.ExecuteNonQuery();
+            //m_dbConnection.Close();
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
