d9136497db69377d55bf819de9ff5dbf6e1a36a2
lukas-hllr <lukas.holler@live.de>
2023-02-27 17:32:46 +0100

    tested signal propagation value

---
+++ backend/Dhbw positioning System Backend/Calculation/RSSItoDistanceConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dhbw_positioning_System_Backend.Calculation;
+
+public class RSSItoDistanceConverter
+{
+    private const double SignalPropagation = 3.0; //Usually between 2 and 4.3, has to be tested.  
+    private const double RssiAtOneMeter = -32; //Measured reference-RSSI at a distance of 1m  
+    public static double Convert(double measuredRssi)
+    {
+        return Math.Pow(10, (RssiAtOneMeter - measuredRssi) / (10 * SignalPropagation));
+    }
+}
