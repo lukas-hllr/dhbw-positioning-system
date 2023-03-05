64f9de34b0ba8dbbe96bd19b6b21cf8e05206d47
Leonneu <52626756+Leonneu@users.noreply.github.com>
2023-03-05 12:21:20 +0100

    Refactored and Improved Algorithm

--- backend/Dhbw positioning System Backend/Calculation/Trilateration.cs
+++ backend/Dhbw positioning System Backend/Calculation/Trilateration.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Linq;
 using GeoCoordinatePortable;
 
 namespace Dhbw_positioning_System_Backend.Calculation;
 
 public static class Trilateration
 {
-    public static GeoCoordinate IterativeIntersection(GeoCoordinate[] positions, double[] distances, double epsilon = 2)
+    public static GeoCoordinate IterativeIntersection(GeoCoordinate[] positions, double[] trueDistances, double epsilon = 0.1)
     {
         // Check that the number of positions and distances are the same
-        if (positions.Length != distances.Length)
+        if (positions.Length != trueDistances.Length)
             throw new ArgumentException("The number of positions and distances must be the same.");
 
         // Initialize the position estimate to the centroid of the reference points
@@ -20,46 +21,61 @@ public static class Trilateration
         }
         lat /= positions.Length;
         lon /= positions.Length;
-        GeoCoordinate position = new GeoCoordinate(lat, lon);
-
-        // Iterate until the position estimate converges
-        while (true)
+        GeoCoordinate estimatedPosition = new GeoCoordinate(lat, lon);
+        double accuracy=9999, lastAccuracy=99999;
+        // Iterate until the position estimate converges (Which happens when the error is not getting better)
+        while (GetAccuracyImprovement(accuracy,lastAccuracy)<epsilon)
         {
             // Calculate the distances from the current estimate to each reference point
-            double[] currentDistances = new double[distances.Length];
-            for (int i = 0; i < distances.Length; i++)
-            {
-                currentDistances[i] = positions[i].GetDistanceTo(position);
-            }
+            double[] calculatedDistances = GetCalculatedDistances(positions, estimatedPosition);
 
             // Check if the current estimate meets the required accuracy
-            double error = 0;
-            for (int i = 0; i < distances.Length; i++)
-            {
-                error += Math.Abs(currentDistances[i] - distances[i]);
-            }
-            if (error < epsilon)
-                break;
-
+            lastAccuracy = accuracy;
+            accuracy = CalculatedAccuracy(trueDistances, calculatedDistances);
             // Update the position estimate using a weighted least-squares algorithm
-            double[] weights = new double[distances.Length];
-            double sum = 0;
-            for (int i = 0; i < distances.Length; i++)
-            {
-                weights[i] = 1.0 / currentDistances[i];
-                sum += weights[i];
-            }
-            double latSum = 0, lonSum = 0;
-            for (int i = 0; i < distances.Length; i++)
-            {
-                latSum += weights[i] * positions[i].Latitude;
-                lonSum += weights[i] * positions[i].Longitude;
-            }
-            lat = latSum / sum;
-            lon = lonSum / sum;
-            position = new GeoCoordinate(lat, lon);
+            estimatedPosition = UpdatePosition(positions, calculatedDistances);
         }
+        return estimatedPosition;
+    }
 
-        return position;
+    private static double GetAccuracyImprovement(double error, double lastError)
+    {
+        return Math.Abs(lastError - error);
+    }
+    private static GeoCoordinate UpdatePosition(GeoCoordinate[] positions, double[] calculatedDistances)
+    {
+        int n = calculatedDistances.Length;
+        double[] weights = new double[n];
+        double sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            weights[i] = 1.0 / calculatedDistances[i];
+            sum += weights[i];
+        }
+        double latSum = 0, lonSum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            latSum += weights[i] * positions[i].Latitude;
+            lonSum += weights[i] * positions[i].Longitude;
+        }
+        double lat = latSum / sum;
+        double lon = lonSum / sum;
+        return new GeoCoordinate(lat, lon);
+    }
+
+    private static double CalculatedAccuracy(double[] trueDistances, double[] calculatedDistances)
+    {
+        return trueDistances.Select((t, i) => Math.Abs(calculatedDistances[i] - t)).Sum();
+    }
+
+    private static double[] GetCalculatedDistances(GeoCoordinate[] positions,GeoCoordinate calculatedPosition)
+    {
+        int n = positions.Length; 
+        double[] calculatedDistances = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            calculatedDistances[i] = positions[i].GetDistanceTo(calculatedPosition);
+        }
+        return calculatedDistances;
     }
 }
