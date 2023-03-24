9ea6f94dc2a2b9025ae66f8902d6f4eb3d208cf2
Leon Neumann <lneumann@allegion.com>
2023-03-24 10:47:08 +0100

    Added unit test for faulty request data

--- backend/backend-test/UnitTest1.cs
+++ backend/backend-test/UnitTest1.cs
@@ -20,13 +20,13 @@ public class Tests
             new GeoCoordinate(49.0272082, 8.38570786)
         };
 
-        double[] distances = new double[] {15.7, 4.8, 13.2};
-        
+        double[] distances = new double[] { 15.7, 4.8, 13.2 };
+
         var result = Trilateration.IterativeIntersection(knownRoutersGeoCord, distances, 2);
         Assert.That(result.Latitude, Is.EqualTo(49.02724149333333).Within(0.001));
         Assert.That(result.Longitude, Is.EqualTo(8.385532226666667).Within(0.001));
     }
-    
+
     [Test]
     public void TestLateration2()
     {
@@ -37,7 +37,7 @@ public class Tests
             new GeoCoordinate(49.0271643231129, 8.38584581316988)
         };
 
-        double[] distances = new double[] {5, 10, 18};
+        double[] distances = new double[] { 5, 10, 18 };
         Multilateration multilateration = new Multilateration(knownRoutersGeoCord, distances);
         var result = multilateration.FindOptimalLocation();
         Console.WriteLine(result);
@@ -46,11 +46,36 @@ public class Tests
     }
 
     [Test]
+    public void TestLateration3()
+    {
+        GeoCoordinate[] knownRoutersGeoCord =
+        {
+            new GeoCoordinate(49.0272880184736, 8.38570350114179),
+            new GeoCoordinate(49.0273064641865, 8.38550658162981),
+            new GeoCoordinate(49.0271643231129, 8.38584581316988),
+            new GeoCoordinate(49.0273249104467, 8.38533117269256),
+            new GeoCoordinate(49.0271523880134, 8.38526332626088),
+            new GeoCoordinate(49.0271380365763, 8.38539400717443),
+        };
+
+        double[] distances = new double[]
+        {
+            5.843414133735177, 58.434141337351754, 58.434141337351754, 107.97751623277094, 116.59144011798323,
+            125.89254117941675
+        };
+        Multilateration multilateration = new Multilateration(knownRoutersGeoCord, distances);
+        var result = multilateration.FindOptimalLocation();
+        Console.WriteLine(result);
+        Assert.That(result.Latitude, Is.EqualTo(49.02751575703979).Within(0.0001));
+        Assert.That(result.Longitude, Is.EqualTo(8.386484553128351).Within(0.0001));
+    }
+
+    [Test]
     public void TestRssiConverter()
     {
         var dp = new MeasurementEntityDto()
         {
-            Rssi = -76,
+            Rssi = -87,
             Ssid = "DHBW-KA5"
         };
         var result = RSSItoDistanceConverter.Convert(dp);
