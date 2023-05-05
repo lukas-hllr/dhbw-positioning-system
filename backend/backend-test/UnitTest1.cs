fd9540618000fa5b599e741ed3aa6838dd2b98a7
Leonneu <52626756+Leonneu@users.noreply.github.com>
2023-05-05 16:31:42 +0200

    Made Access in Test Readonly

--- backend/backend-test/UnitTest1.cs
+++ backend/backend-test/UnitTest1.cs
@@ -11,13 +11,11 @@ using NUnit.Framework;
 public class Tests
 {
     private DhbwPositioningSystemDBContext _context;
-
-    //CAREFUL, WORKING WITH PRODUCTION DB!!! TRY TO READ ONLY!!!
     [SetUp]
     public void SetUp()
     {
         var optionsBuilder = new DbContextOptionsBuilder<DhbwPositioningSystemDBContext>()
-            .UseSqlite("Data Source=..\\..\\..\\..\\Dhbw positioning System Backend\\DhbwPositioningSystemDB.db")//Standart Path is in dhbw-positioning-system\backend\backend-test\bin\Debug\net6.0 which is fucked, Idk how to fix so thats why weird navigation.
+            .UseSqlite("Data Source=..\\..\\..\\..\\Dhbw positioning System Backend\\DhbwPositioningSystemDB.db;Mode=ReadOnly")//Standart Path is in dhbw-positioning-system\backend\backend-test\bin\Debug\net6.0 which is fucked, Idk how to fix so thats why weird navigation.
             .Options;
         _context = new DhbwPositioningSystemDBContext(optionsBuilder);
         _context.Database.OpenConnection();
