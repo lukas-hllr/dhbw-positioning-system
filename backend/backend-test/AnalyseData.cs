cb66bc63b000df6552401c783655605bfb865fd9
Lukas Holler <lukas.holler@live.de>
2023-05-13 00:10:58 +0200

    added new Test and csv export

--- backend/backend-test/AnalyseData.cs
+++ backend/backend-test/AnalyseData.cs
@@ -1,6 +1,9 @@
-﻿using Dhbw_positioning_System_Backend;
+﻿using System.Text;
+using Dhbw_positioning_System_Backend;
 using Dhbw_positioning_System_Backend.Calculation;
 using Dhbw_positioning_System_Backend.Model;
+using Dhbw_positioning_System_Backend.Model.dto;
+using Dhbw_positioning_System_Backend.Controllers;
 using GeoCoordinatePortable;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +12,8 @@ namespace backend_test.TestData;
 public class AnalyseData
 {
     private DhbwPositioningSystemDBContext _context;
+    private RayCastingAlgorithm _rayCastingAlgorithm;
+    private LocationController _locationController;
 
     [SetUp]
     public void SetUp()
@@ -16,9 +21,13 @@ public class AnalyseData
         var optionsBuilder = new DbContextOptionsBuilder<DhbwPositioningSystemDBContext>()
             .UseSqlite(
                 "Data Source=..\\..\\..\\..\\Dhbw positioning System Backend\\DhbwPositioningSystemDB.db;Mode=ReadOnly") //Standart Path is in dhbw-positioning-system\backend\backend-test\bin\Debug\net6.0 which is fucked, Idk how to fix so thats why weird navigation.
+            .UseLazyLoadingProxies()
             .Options;
         _context = new DhbwPositioningSystemDBContext(optionsBuilder);
         _context.Database.OpenConnection();
+
+        _rayCastingAlgorithm = new RayCastingAlgorithm();
+        _locationController = new LocationController(_context, _rayCastingAlgorithm);
     }
 
     [Test]
@@ -87,5 +96,91 @@ public class AnalyseData
         }
 
         return filtered;
+    }
+
+    [Test]
+    public void testDistancesToGroundTruthAndLocationServices(){
+
+        var res = calcDbMeasurements();
+
+        Assert.AreEqual(0, 0);
+    }
+
+    private Dictionary<string, dynamic> calcDbMeasurements(bool writeCsv = true){
+        var result = new Dictionary<string, dynamic>();
+
+        var groundTruthList = new List<GeoCoordinate>();
+        var calculatedList = new List<LocationDto>();
+        var locationServicesList = new List<GeoCoordinate>();
+        var distanceToCalculatedList = new List<double>();
+        var distanceToLocationServicesList = new List<double>();
+
+        foreach (var m in _context.Measurement)
+        {
+            var measurementEntities = new List<MeasurementEntityDto>();
+
+            foreach(var me in m.MeasurementEntity)
+            {
+                measurementEntities.Add(new MeasurementEntityDto(me));
+            }
+
+            var groundTruth = new GeoCoordinate(m.LatitudeGroundTruth, m.LongitudeGroundTruth);
+            var calculated = _locationController.GetLocation(measurementEntities).Value!;
+            var locationServices = new GeoCoordinate((double)m.LatitudeHighAccuracy, (double)m.LongitudeHighAccuracy) { VerticalAccuracy = (double)m.AccuracyHighAccuracy };
+
+            groundTruthList.Add(groundTruth);
+            locationServicesList.Add(locationServices);
+            calculatedList.Add(calculated);
+            distanceToCalculatedList.Add(new GeoCoordinate(calculated.Latitude, calculated.Longitude).GetDistanceTo(groundTruth));
+            distanceToLocationServicesList.Add(new GeoCoordinate(calculated.Latitude, calculated.Longitude).GetDistanceTo(locationServices));
+
+        }
+
+        result.Add("groundTruth", groundTruthList);
+        result.Add("calculated", calculatedList);
+        result.Add("locationServices", locationServicesList);
+        result.Add("distanceToCalculated", distanceToCalculatedList);
+        result.Add("distanceToLocationServices", distanceToLocationServicesList);
+
+        if (writeCsv)
+        {
+            writeResultsToCsv(result);
+        }
+
+        return result;
+    }
+
+    private void writeResultsToCsv(Dictionary<string, dynamic> res)
+    {
+        var filePath = Path.Join(Directory.GetParent(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory)).Parent.FullName, "results.csv");
+        var csv = new StringBuilder();
+        var csvHeader = "lat,lng,latCalculated,lngCalculated,accCalculated,distanceToCalculated,latLocationServices,lngLocationServices,accCalculated,distanceToLocationServices";
+        csv.AppendLine(csvHeader);
+
+        var groundTruthList = res["groundTruth"]! as List<GeoCoordinate>;
+        var calculatedList = res["calculated"]! as List<LocationDto>;
+        var locationServicesList = res["locationServices"] as List<GeoCoordinate>;
+        var distanceToCalculatedList = res["distanceToCalculated"] as List<double>;
+        var distanceToLocationServicesList = res["distanceToLocationServices"] as List<double>;
+
+        for (int i = 0; i < groundTruthList.Count; i++)
+        {
+            var newLine = string.Format(
+                "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}",
+                groundTruthList.ElementAt(i).Latitude,
+                groundTruthList.ElementAt(i).Longitude,
+                calculatedList.ElementAt(i).Latitude,
+                calculatedList.ElementAt(i).Longitude,
+                calculatedList.ElementAt(i).Accuracy,
+                distanceToCalculatedList.ElementAt(i),
+                locationServicesList.ElementAt(i).Latitude,
+                locationServicesList.ElementAt(i).Longitude,
+                locationServicesList.ElementAt(i).VerticalAccuracy,
+                distanceToLocationServicesList.ElementAt(i)
+            );
+            csv.AppendLine(newLine);
+        }
+
+        File.WriteAllText(filePath, csv.ToString());
     }
 }
