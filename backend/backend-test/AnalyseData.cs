63165353a947d542f735029326b0526ea3018aec
Leonneu <52626756+Leonneu@users.noreply.github.com>
2023-05-12 08:09:50 +0200

    Added Test Class

---
+++ backend/backend-test/AnalyseData.cs
@@ -0,0 +1,20 @@
+﻿using Dhbw_positioning_System_Backend;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_test.TestData;
+
+public class AnalyseData
+{
+    private DhbwPositioningSystemDBContext _context;
+    [SetUp]
+    public void SetUp()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DhbwPositioningSystemDBContext>()
+            .UseSqlite("Data Source=..\\..\\..\\..\\Dhbw positioning System Backend\\DhbwPositioningSystemDB.db;Mode=ReadOnly")//Standart Path is in dhbw-positioning-system\backend\backend-test\bin\Debug\net6.0 which is fucked, Idk how to fix so thats why weird navigation.
+            .Options;
+        _context = new DhbwPositioningSystemDBContext(optionsBuilder);
+        _context.Database.OpenConnection();
+    } 
+    
+    
+}
